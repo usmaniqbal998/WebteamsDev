81fbdb3571213b11785b27c2e74b69601df337b7
Prashant <prashantgmr7@gmail.com>
2022-12-20 20:49:08 +0545

    add svg icons and import it in box

--- src/components/organisms/InfoBox.jsx
+++ src/components/organisms/InfoBox.jsx
@@ -1,22 +1,23 @@
 import { GatsbyImage, StaticImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
-import Text from '../atoms/Text'
 import Typography from '../atoms/Text'
 
 const InfoBox = ({
   title,
   content,
   icon,
+  iconImg,
   className = 'flex flex-col gap-6 text-left text-white',
 }) => {
-  const image = getImage(icon)
+  const image = getImage(iconImg)
   return (
     <div className={className}>
-      {icon && (
+      {iconImg && (
         <div>
           <GatsbyImage image={image} alt={title} />
         </div>
       )}
+      {icon && icon}
       <div>
         <Typography variant="h5" className="mb-4" color="current">
           {title}
