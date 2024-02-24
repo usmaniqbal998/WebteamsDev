f82a8300248464c6b6b29d60baa3a051b8185804
JonRC <jonarc06@gmail.com>
2024-02-24 14:38:25 -0300

    create BracketText

---
+++ src/Components/BracketText.tsx
@@ -0,0 +1,7 @@
+export function BracketText() {
+  return <div className="text-white flex w-[468px]">
+    <span className="text-white text-8xl">&#123;</span>
+    <span className="text-white max-h-[6rem] overflow-hidden pt-4">Embark on your product journey with our elite and precise agile Squad.</span>
+    <span className="text-white text-8xl">&#125;</span>
+  </div>
+}
