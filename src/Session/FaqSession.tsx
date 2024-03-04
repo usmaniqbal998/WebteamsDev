adbf757a62ff799e73afc4b8b04c7a7c64f5aad6
Jonathan Rodrigues Cardoso <jonarc06@gmail.com>
2024-03-04 20:53:40 -0300

    feat: explorer session and faq session

---
+++ src/Session/FaqSession.tsx
@@ -0,0 +1,39 @@
+import { Faq, FaqItem } from '@/Components/faq'
+
+export const FaqSession = () => {
+  const faqItems: FaqItem[] = [
+    {
+      question: 'How can I get started with my project?',
+      answer: `Simply reach out to us through our contact page, share your project details, and we'll guide you through the seamless process of initiating your web/app development and UX/UI project.`
+    },
+    {
+      question: `What industries do you cater to?`,
+      answer: `We take pride in our versatility. Our expertise extends across various industries, including but not limited to technology, healthcare, finance, e-commerce, and education. Feel free to inquire about your specific industry, and we'll be happy to discuss how we can tailor our services to meet your unique needs.`
+    },
+    {
+      question: `What services do you offer?`,
+      answer: `We offer comprehensive web and app development services along with UX/UI design expertise. Our services encompass everything from initial consultation and needs assessment to design, development, testing, and ongoing support. Whether you need a new website, a mobile app, or a revamp of your user experience, we've got you covered. Explore our full range of services to find the solutions that best suit your project goals.`
+    },
+    {
+      question: `How does the development process work?`,
+      answer: `Our development process involves initial consultation, needs assessment, design, development, testing, and ongoing support. We collaborate closely with you at every stage to ensure your project evolves seamlessly from concept to reality.`
+    },
+    {
+      question: `What is the typical timeline for a project?`,
+      answer: `Project timelines vary based on factors like complexity and features. Following our initial consultation and needs assessment, we provide a customized timeline in our project proposal. Rest assured, we prioritize efficiency without compromising quality to deliver your project promptly.`
+    },
+    {
+      question: `Can you handle both small and large-scale projects?`,
+      answer: `Absolutely. We're equipped to handle projects of all sizes with the same dedication and expertise, ensuring successful outcomes for both small and large-scale initiatives.`
+    }
+  ]
+
+  return (
+    <div>
+      <h2 className='mb-8 flex flex-col items-start justify-center text-4xl'>
+        Frequently asked questions
+      </h2>
+      <Faq faqItems={faqItems} />
+    </div>
+  )
+}
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,20 +1,5 @@
-import { CommentCard } from '@/Components/CommentCard'
-import { Footer } from '@/Components/Footer'
-import { RecentWorkGallery } from '@/Components/RecentWorkGallery'
-import { Faq, FaqItem } from '@/Components/faq'
-import { RecentWork } from '@/Session/RecentWork'
-
-const faqItems: FaqItem[] = [
-  {
-    question: 'How can I get started with my project?',
-    answer: `Simply reach out to us through our contact page, share your project details, and we'll guide you through the seamless process of initiating your web/app development and UX/UI project.`
-  },
-  {
-    question: `What industries do you cater to?`,
-    answer: `We take pride in our versatility. Our expertise extends across various industries, including but not limited to technology, healthcare, finance, e-commerce, and education. Feel free to inquire about your specific industry, and we'll be happy to discuss how we can tailor our services to meet your unique needs.`
-  }
-]
+import { Explore } from '@/Session/Explorer'
 
 export default function test() {
-  return <div>lsdkfj</div>
+  return <Explore />
 }
