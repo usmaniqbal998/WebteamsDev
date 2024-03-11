030d2c465ceb25cad2f443ff4c86692941b4840c
Jonathan Rodrigues Cardoso <jonarc06@gmail.com>
2024-03-11 09:05:49 -0300

    feat: finished contact us desktop and mobile

--- src/Components/ui/input.tsx
+++ src/Components/ui/input.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 
 import { cn } from '@/Util'
+import { FieldErrors } from 'react-hook-form'
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  error?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error?: FieldErrors<Record<string, any>>
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -14,14 +16,15 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            'flex h-10 w-full border-b-2 bg-black px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            'flex h-10 autofill:bg-black autofill:text-white w-full border-b-2 bg-black px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
             className
           )}
           ref={ref}
           {...props}
         />
-        <span className='h-[1ch] w-full text-xs text-red-700'>
-          {error}fkdkdsjflkdsjaklfjkj
+
+        <span className='mt-2 block h-[1ch] min-h-[1ch] w-full text-xs text-red-600'>
+          {error?.[props.name || '']?.message?.toString()}
         </span>
       </div>
     )
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,56 +1,134 @@
 'use client'
 
 import { Input } from '@/Components/ui/input'
-import { useForm } from 'react-hook-form'
-import {} from '@hookform/resolvers/zod'
+import {
+  SubmitHandler,
+  UseFormHandleSubmit,
+  useForm,
+  useFormState
+} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { sendContactMessage } from '@/app/contact/sendContactMessage'
+import { useState } from 'react'
+import { cn } from '@/Util'
 
 const numbersRegex = /^\d+$/
 
 const formSchema = z.object({
-  name: z.string().min(1),
-  email: z.string().email(),
-  phone: z.string().refine(value => numbersRegex.test(value)),
-  aboutProject: z.string().min(1)
+  name: z.string().min(1, 'A name is required.'),
+  email: z.string().email('Invalid email.'),
+  phone: z
+    .string()
+    .refine(
+      (value: unknown) => typeof value === 'string' && numbersRegex.test(value),
+      'Should be numbers only.'
+    ),
+  aboutProject: z.string().min(1, 'A message is required.')
 })
 
+type FormSchema = z.infer<typeof formSchema>
+
 export default function Contract() {
-  const { register, formState } = useForm()
+  const { register, formState, handleSubmit, reset } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema)
+  })
+
+  const [pending, setPending] = useState(false)
+
   const { errors } = formState
+
+  const handleContactForm: SubmitHandler<FormSchema> = async (
+    formData,
+    event
+  ) => {
+    event?.preventDefault()
+    setPending(true)
+    await sendContactMessage()
+
+    setPending(false)
+    reset()
+  }
 
   return (
     <div
-      className='grid size-full grid-cols-12 grid-rows-1 group-first/footer:bg-red-500'
+      className={cn(
+        'grid size-full grid-cols-12 grid-rows-1 group-first/footer:bg-red-500',
+        'sm:grid-cols-1 sm:grid-rows-12 sm:justify-center sm:items-center sm:gap-y-5 sm:mt-4'
+      )}
       data-page='contact'
     >
-      <div className='col-span-5 flex size-[200%] -translate-x-1/2 -translate-y-1/2 items-end justify-end bg-neutral-800'>
-        <div className='flex h-[45%] w-1/2 items-start'>
-          <span className='block w-8/12 text-[84px] leading-[1.05] [&_mark]:font-semibold [&_mark]:text-green-500'>
-            <mark>&quot;</mark>We love to hear from you<mark>&quot;</mark>
+      <div
+        className={cn(
+          'col-span-5 flex size-[200%] -translate-x-1/2 -translate-y-1/2 items-end justify-end bg-neutral-800',
+          'sm:col-span-1 sm:row-span-3 sm:translate-y-0 -translate-x-1/2 sm:h-full sm:w-[400%]'
+        )}
+      >
+        <div className='flex h-[45%] w-1/2 items-start sm:h-full sm:items-center sm:text-center'>
+          <span className='block w-8/12 text-[84px] leading-[1.05] lg:text-6xl md:text-3xl sm:w-6/12 [&_mark]:font-semibold [&_mark]:text-green-500'>
+            <mark>&quot;</mark>We love to <br className='hidden sm:block' />{' '}
+            hear from you<mark>&quot;</mark>
           </span>
         </div>
       </div>
-      <div className='col-span-7 flex w-full flex-col items-start justify-center gap-16 pl-28'>
-        <form action='' className='flex w-3/6 flex-col gap-y-[2vh]'>
-          <Input type='text' placeholder='Name' {...register('name')} />
-          <Input type='email' placeholder='Email' {...register('email')} />
-          <Input type='number' placeholder='Phone' {...register('phone')} />
+      <div
+        className={cn(
+          'col-span-7 flex w-full flex-col items-start justify-center gap-16 pl-28 sm:gap-8',
+          'sm:row-span-9 sm:pl-0'
+        )}
+      >
+        <form
+          onSubmit={handleSubmit(handleContactForm)}
+          className='flex w-3/6 flex-col gap-y-[2vh] sm:w-full'
+          method='POST'
+        >
           <Input
+            disabled={pending}
+            type='text'
+            placeholder='Name'
+            {...register('name')}
+            error={errors}
+          />
+          <Input
+            disabled={pending}
+            type='email'
+            placeholder='Email'
+            {...register('email')}
+            error={errors}
+          />
+          <Input
+            disabled={pending}
+            type='number'
+            placeholder='Phone'
+            {...register('phone')}
+            error={errors}
+          />
+          <Input
+            disabled={pending}
             type='text'
             placeholder='Tell us about your project'
             {...register('aboutProject')}
+            error={errors}
           />
           <button
+            disabled={pending}
+            data-pending={pending}
             type='submit'
-            className='w-min rounded-full bg-white px-4 py-2 text-sm font-semibold text-black'
+            className='flex h-10 w-3/12 items-center justify-center gap-4 rounded-full bg-white text-sm font-semibold text-black transition-transform duration-500 data-[pending=true]:bg-neutral-400'
           >
-            Send
+            <span data-pending={pending} className='data-[pending=true]:hidden'>
+              Send
+            </span>
+            <div
+              data-pending={pending}
+              className='loader w-2/12 data-[pending=false]:hidden'
+            ></div>
           </button>
         </form>
 
-        <div className='flex flex-col gap-4 '>
+        <div className='flex flex-col gap-4 sm:gap-2'>
           <span className='text-neutral-400'>Support</span>
-          <span>info@webteams.com</span>
+          <span className='text-xl sm:text-base'>info@webteams.com</span>
         </div>
       </div>
     </div>
