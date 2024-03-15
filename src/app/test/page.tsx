de371995217dec1607223c2c18fa8d5f073f76a7
JonRC <jonarc06@gmail.com>
2024-03-14 22:23:05 -0300

    feat/finalized React Native Advantages

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSideBar } from '@/Components/HeroSideBar'
+import { ReactNativeAdvantages } from '@/Components/ReactNativeAdvantages'
 import { DesignHero } from '@/Session/DesignHero'
 
 export default function test() {
-  return <DesignHero />
+  return <ReactNativeAdvantages />
 }
