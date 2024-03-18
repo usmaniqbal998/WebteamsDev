bdb26b38e238b1f2a7e8a0f8c584771315a151e4
JonRC <jonarc06@gmail.com>
2024-03-18 12:06:03 -0300

    feat:finished WebAppDevHero

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSideBar } from '@/Components/HeroSideBar'
 import { DesignHero } from '@/Session/DesignHero'
+import { WebAppDevHero } from '@/Session/WebAppDevHero'
 
 export default function test() {
-  return <DesignHero />
+  return <WebAppDevHero />
 }
