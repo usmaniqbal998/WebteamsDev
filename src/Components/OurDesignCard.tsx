30b4a827600d25d1fcae99dcefb59d5e2fdae523
JonRC <jonarc06@gmail.com>
2024-03-24 13:37:42 -0300

    feat: web page created

--- src/Components/OurDesignCard.tsx
+++ src/Components/OurDesignCard.tsx
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-type OurDesignCardProps = {
-  title: string
-  steps: {
-    subtitle: string
-    text: string
-  }[]
-=======
 import { ReactNode } from 'react'
 
 export type Step = {
@@ -16,24 +8,16 @@ export type Step = {
 type OurDesignCardProps = {
   title: ReactNode
   steps: Step[]
->>>>>>> Stashed changes
 }
 
 export function OurDesignCard(props: OurDesignCardProps) {
   return (
-<<<<<<< Updated upstream
-    <div className='flex w-screen flex-col gap-10 bg-zinc-900 px-72 '>
-      <h2 className='text-8xl '>
-        Our design <br /> process
-      </h2>
-=======
     <div className='mt-14 flex w-screen flex-col gap-10 bg-zinc-900 px-72 '>
       <h2 className='mt-12 text-8xl '>{props.title}</h2>
->>>>>>> Stashed changes
-      <div className=' flex w-full flex-row items-start justify-center gap-10'>
+      <div className=' flex w-full flex-row items-start justify-center gap-4'>
         {props.steps.map((step, index) => (
           <>
-            <div className='flex w-32 flex-col gap-2'>
+            <div className='flex w-36 flex-col gap-2'>
               <span className='flex size-11 items-center justify-center rounded-full bg-green-300 text-xl text-black'>
                 {index + 1}
               </span>
